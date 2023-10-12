4b140153735344f3556a836530eb05d872bf122a
alrudakov <arud@velesbsd.com>
2023-10-12 18:21:48 +0300

    ddd

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ user_proxy = UserProxyAgent(name="user_proxy")
 # the assistant receives a message from the user, which contains the task description
 user_proxy.initiate_chat(
     assistant,
-    message="""Сделай сайт визитку посвященную искуственному интеллекту и разверни ее у меня на сервере и дай мне URL что бы я мог посмотреть""",
+    message="""Сделай сайт визитку посвященную искуственному интеллекту и разверни ее у меня на сервере и дай мне URL что бы я мог посмотреть и отвечай на русском""",
 )
